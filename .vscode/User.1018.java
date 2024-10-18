e3615397cfd8718acb3ee3695a108fe0f491afbe
SK0403-YI <chansora43@gmail.com>
2024-10-18 21:23:00 +0900

    課題の提出

---
+++ .vscode/User.1018.java
@@ -0,0 +1,28 @@
+package member.bean;
+
+public class User {
+	// ここに作成
+	String name;
+	String email;
+
+	User(String name,String email){
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getName(){
+		return name;
+
+	}
+
+	public String getEmail(){
+		return email;
+	}
+
+	public toString extends ???????
+
+	System.out.println("名前:" + name);
+	System.out.println("Emailアドレス:" + email);
+
+	// ここまで
+}
